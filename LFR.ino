f49b9b0c1745bf73599f65c4e657f0f3e66713e0
naeemsadik <67340415+naeemsadik@users.noreply.github.com>
2023-10-07 09:50:34 +0600

    BasicCodeForForwardBackwardLeft&Right2

--- LFR.ino
+++ LFR.ino
@@ -4,13 +4,19 @@
 #define LMneg 10
 #define EnableRM A0
 #define EnableLM A1
+#define s1 0
+#define s2 1
+#define s3 2
+#define s4 3
+#define s5 4
+#define s5 5
 
 int RMspeed = 200;
 int LMspeed = 200;
 
 
 void forward() {
-  Serial.println("Forward");
+  Serial.println("Going Forward...");
   digitalWrite(RMpos, HIGH);
   digitalWrite(RMneg, LOW);
   digitalWrite(LMpos, LOW);
@@ -20,7 +26,7 @@ void forward() {
 }
 
 void backward() {
-  Serial.println("Backward");
+  Serial.println("Going Backward...");
   digitalWrite(RMpos, LOW);
   digitalWrite(RMneg, HIGH);
   digitalWrite(LMpos, HIGH);
@@ -29,8 +35,8 @@ void backward() {
   analogWrite(EnableLM, LMspeed);
 }
 
-void left() {
-  Serial.println("Left");
+void right() {
+  Serial.println("Turning Right...");
   digitalWrite(RMpos, LOW);
   digitalWrite(RMneg, LOW);
   digitalWrite(LMpos, LOW);
@@ -39,8 +45,8 @@ void left() {
   analogWrite(EnableLM, LMspeed);
 }
 
-void right() {
-  Serial.println("Right");
+void left() {
+  Serial.println("Turning Left...");
   digitalWrite(RMpos, HIGH);
   digitalWrite(RMneg, LOW);
   digitalWrite(LMpos, LOW);
@@ -50,7 +56,7 @@ void right() {
 }
 
 void stop() {
-  Serial.println("Stop");
+  Serial.println("Stoping.....");
   digitalWrite(RMpos, LOW);
   digitalWrite(RMneg, LOW);
   digitalWrite(LMpos, LOW);
@@ -68,16 +74,23 @@ void setup()
   pinMode(LMneg,OUTPUT);
   pinMode(EnableRM, OUTPUT);
   pinMode(EnableLM, OUTPUT);
+  pinMode(s1, INPUT);
+  pinMode(s2, INPUT);
+  pinMode(s3, INPUT);
+  pinMode(s4, INPUT);
+  pinMode(s5, INPUT);
+  pinMode(s6, INPUT);
 }
 
 void loop()
 {
+  Serial.println(digitalread(s1));
   forward();
-  delay(1000);
+  delay(10000);
   stop();
   delay(1000);
   backward();
-  delay(1000);
+  delay(10000);
   stop();
   delay(1000);
   right();
